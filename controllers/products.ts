dc9bc4a637095d185c72ce3c27460c837f62c642
Ramadan <r.mohey@rytalo.com>
2020-06-03 19:08:13 +0200

    iitial commit

---
+++ controllers/products.ts
@@ -0,0 +1,135 @@
+
+import { v4 } from 'https://deno.land/std/uuid/mod.ts'
+import { Product } from '../types.ts'
+
+
+let products: Product[] = [
+    {
+        id: "1",
+        name: "Product One",
+        description: "This is product one",
+        price: 29.99
+    },
+    {
+        id: "2",
+        name: "Product Two",
+        description: "This is product two",
+        price: 24.99
+    },
+    {
+        id: "3",
+        name: "Product Three",
+        description: "This is product three",
+        price: 9.99
+    }
+]
+
+
+// @desc Get all products
+// @routes Get /api/v1/products
+
+const getProducts = ({ response }: { response: any })=> {
+    response.body = {
+        success: true,
+        data: products
+    }
+}
+
+// @desc Get single product
+// @routes Get /api/v1/products/:id
+
+const getProduct = ({ params, response }: { params : {id: string }, response: any })=> {
+   const product: Product | undefined = products.find(p => p.id === params.id);
+
+   if(product){
+       response.status = 200;
+       response.body = {
+           success: true,
+           data: product
+       }
+   }else {
+       response.status = 404;
+       response.body = {
+        success: false,
+        msg: "No product found"
+       }
+   }
+}
+
+// @desc add single product
+// @routes Get /api/v1/products
+
+const addProduct = async  ({ request , response }: { request: any , response: any })=> {
+    const body = await request.body();
+
+    if(!request.hasBody){
+        response.status = 400;
+        response.body = {
+            success: false,
+            msg: 'No data'
+        }
+    } else {
+        const product: Product = body.value;
+        product.id =v4.generate();
+        products.push(product);
+        response.status = 201
+        response.body = {
+            success: true,
+            data: product
+        }
+
+
+    }
+     
+}
+
+// @desc update single product
+// @routes Get /api/v1/products/:id
+
+const updateProduct = async ({ params, request, response }: { params: {id: string}, request: any, response: any })=> {
+    const product: Product | undefined = products.find(p => p.id === params.id);
+
+   if(product){
+       const body = await request.body();
+       const updateData: {name?: string, description?: string, price?:number } = body.value;
+       products = products.map(p => p.id === params.id ? {...p, ...updateData} : p)
+
+       response.status = 200;
+       response.body = {
+           success: true,
+           data: products
+       }
+
+   }else {
+       response.status = 404;
+       response.body = {
+        success: false,
+        msg: "No product found"
+       }
+   }
+}
+
+
+// @desc delete single product
+// @routes delete /api/v1/products/:id
+
+const deleteProduct = ({ params, response }: { params: {id: string},response: any })=> {
+
+   if(params.id){
+       products = products.filter(p => p.id !== params.id)
+       response.status = 200;
+       response.body = {
+           success: true,
+           data: products
+       }
+
+   }else {
+       response.status = 404;
+       response.body = {
+        success: false,
+        msg: "No product found"
+       }
+   }
+}
+
+export { getProducts, getProduct ,addProduct, updateProduct, deleteProduct };
